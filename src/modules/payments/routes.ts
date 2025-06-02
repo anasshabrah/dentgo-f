8cd58843f1d1d5980962d633568a3907c88a522d
Anass Habrah <syriandealer@gmail.com>
2025-06-03 01:06:01 +0300

    fix issues

--- src/modules/payments/routes.ts
+++ src/modules/payments/routes.ts
@@ -1,19 +1,21 @@
 import React, { lazy } from "react";
 
-const PaymentMethod     = lazy(() => import("../../pages/PaymentMethod"));
-const AddNewCard        = lazy(() => import("../../pages/AddNewCard"));
-const SelectPayment     = lazy(() => import("../../pages/SelectPaymentMethod"));
-const SubscriptionPay   = lazy(() => import("../../pages/SubscriptionPayment"));
-const PlusSubscription  = lazy(() => import("../../pages/PlusSubscription"));
-const CancelSubscription= lazy(() => import("../../pages/CancelSubscription"));
+const PaymentMethod = lazy(() => import("../../pages/PaymentMethod"));
+const AddNewCard = lazy(() => import("../../pages/AddNewCard"));
+const SelectPayment = lazy(() => import("../../pages/SelectPaymentMethod"));
+const CancelSubscription = lazy(() => import("../../pages/CancelSubscription"));
+const SubscriptionPayment = lazy(() => import("../../pages/SubscriptionPayment"));
+const BankCards = lazy(() => import("../../pages/BankCards"));
+const PlusSubscription = lazy(() => import("../../pages/PlusSubscription"));
 
 const paymentRoutes = [
-  { path: "PaymentMethod",       element: <PaymentMethod /> },
-  { path: "AddNewCard",          element: <AddNewCard /> },
-  { path: "SelectPaymentMethod", element: <SelectPayment /> },
-  { path: "SubscriptionPayment", element: <SubscriptionPay /> },
-  { path: "PlusSubscription",    element: <PlusSubscription /> },
-  { path: "CancelSubscription",  element: <CancelSubscription /> },
+  { path: "payment-method", element: <PaymentMethod /> },
+  { path: "add-new-card", element: <AddNewCard /> },
+  { path: "select-payment-method", element: <SelectPayment /> },
+  { path: "cancel-subscription", element: <CancelSubscription /> },
+  { path: "subscription-payment", element: <SubscriptionPayment /> },
+  { path: "bank-cards", element: <BankCards /> },
+  { path: "plus-subscription", element: <PlusSubscription /> },
 ];
 
 export default paymentRoutes;
