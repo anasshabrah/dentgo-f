8d76c61dca53bb8497904070e640fbb77fa48300
Anass Habrah <syriandealer@gmail.com>
2025-06-03 02:15:53 +0300

    fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -8,9 +7,8 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      // any request to “/api/…” will get forwarded to your backend
       "/api": {
-        target: import.meta.env.VITE_SERVER_URL,
+        target: process.env.VITE_SERVER_URL,
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, "/api"),
