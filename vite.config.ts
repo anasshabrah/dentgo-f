f7b527fdf13e70504d5d314e5a25a7ecb3c1ed59
Anass Habrah <syriandealer@gmail.com>
2025-06-03 01:46:22 +0300

    fix issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
   server: {
-    port: 3000
-  }
+    port: 3000,
+  },
 });
