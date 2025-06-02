48f4146f7349416f4756da086e08b95b748cdee3
Anass Habrah <syriandealer@gmail.com>
2025-06-03 01:52:22 +0300

    fix issues

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { ModalProvider } from "../context/ModalContext";
-import RoutesConfig from "./app/routes";
+import RoutesConfig from "./routes";
 import Loader from "../components/ui/Loader";
 
 export default function App() {
