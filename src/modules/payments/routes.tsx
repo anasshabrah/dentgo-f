2391649c71e748dc0f4017657434339cef966052
Anass Habrah <syriandealer@gmail.com>
2025-06-03 01:20:24 +0300

    fix issues

--- src/modules/payments/routes.tsx
+++ src/modules/payments/routes.tsx
@@ -8,11 +8,16 @@ const SubscriptionPayment = lazy(() => import("../../pages/SubscriptionPayment")
 const BankCards = lazy(() => import("../../pages/BankCards"));
 const PlusSubscription = lazy(() => import("../../pages/PlusSubscription"));
 
-const withSuspense = (Component: React.LazyExoticComponent<React.ComponentType<any>>) => (
-  <Suspense fallback={<div>Loading...</div>}>
-    <Component />
-  </Suspense>
-);
+// Proper functional wrapper component:
+const withSuspense = (
+  Component: React.LazyExoticComponent<React.ComponentType<any>>
+) => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Component />
+    </Suspense>
+  );
+};
 
 const paymentRoutes = [
   { path: "payment-method", element: withSuspense(PaymentMethod) },
