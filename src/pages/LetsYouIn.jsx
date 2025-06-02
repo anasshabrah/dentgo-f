1e6a41140d17eef765a7dcf3f0b0570e9fc47891
Anass Habrah <syriandealer@gmail.com>
2025-06-02 12:52:51 +0300

    fix issues

--- src/pages/LetsYouIn.jsx
+++ src/pages/LetsYouIn.jsx
@@ -156,7 +156,7 @@ export default function LetsYouIn() {
       </div>
 
       {/* Display only the dotsPatternBottom image in the lower third */}
-      <div className="absolute bottom-0 left-0 w-full h-40 overflow-hidden">
+      <div className="absolute bottom-0 left-0 w-full h-1/3 overflow-hidden">
         <img
           src={dentaiBottom}
           alt="Dental AI graphic"
