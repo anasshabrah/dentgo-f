d3477b73397b169f85c4212b61a6735447a4b86b
Anass Habrah <syriandealer@gmail.com>
2025-06-09 04:55:04 +0300

    fix issues

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+// src/config.ts
 export const API_BASE: string = import.meta.env.VITE_SERVER_URL ?? "";
 export const STRIPE_PK: string = import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY ?? "";
 export const GOOGLE_CLIENT_ID: string = import.meta.env.VITE_GOOGLE_CLIENT_ID ?? "";
